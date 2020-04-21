1e9d42d6492cc313e01ff2d3caae1bb6951df7c1
Ismail NORRI <ismail.norri.24@gmail.com>
2020-04-21 11:54:34 +0000

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Date_And_Time_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //My Current Time in Morocco
+            DateTime dateTime = DateTime.UtcNow;
+
+            //Convert to a time zone
+
+            TimeZoneInfo systemTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Tokyo Standard Time");
+            DateTime TokyoTime = TimeZoneInfo.ConvertTime(dateTime, systemTimeZone);
+            
+            //Rendering the times to Console 
+            Console.WriteLine(dateTime);
+            Console.WriteLine(systemTimeZone);
+            Console.WriteLine(TokyoTime);
+        }
+    }
+}
